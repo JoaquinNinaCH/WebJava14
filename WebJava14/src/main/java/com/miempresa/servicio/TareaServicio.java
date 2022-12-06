d60bc7215280d769aa1e59f08e5a8b8582601dbf
JoaquinNinaCH <102237192+JoaquinNinaCH@users.noreply.github.com>
2022-12-06 08:33:22 -0500

    Add files via upload

---
+++ WebJava14/src/main/java/com/miempresa/servicio/TareaServicio.java
@@ -0,0 +1,42 @@
+package com.miempresa.servicio;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.miempresa.interfaceServicio.*;
+import com.miempresa.interfaces.ITarea;
+import com.miempresa.modelo.Tarea;
+
+@Service
+public class TareaServicio implements ITareaServicio{
+
+	@Autowired
+	private ITarea repo;
+	
+	@Override
+	public List<Tarea> listar() {
+		return (List<Tarea>)repo.findAll();
+	}
+
+	@Override
+	public Optional<Tarea> listarId(int id) {
+		return repo.findById(id);
+	}
+
+	@Override
+	public int guardar(Tarea p) {
+		Tarea em = repo.save(p);
+		if(!em.equals(null)) {
+			return 1;
+		}
+		return 0;
+	}
+
+	@Override
+	public void borrar(int id) {
+		repo.deleteById(id);
+		
+	}
+
+
+}
